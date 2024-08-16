8ba823a5aaed27f99a084332d44882b36490c090
drozdovas <dmitrijus.drozdovas@gmail.com>
2024-08-16 09:43:26 +0100

    Init project

---
+++ src/main/java/com/drozdovas/redactionserviceapplication/RedactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.drozdovas.redactionserviceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedactionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drozdovas/redactionserviceapplication/RedactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drozdovas.redactionserviceapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedactionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
